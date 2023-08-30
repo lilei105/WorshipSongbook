c25766ff81a29bfff36fec15ab893851169f46b7
Henry Li <lilei105@hotmail.com>
2023-08-30 13:49:32 +0800

    首页

---
+++ src/app/Headline.js
@@ -0,0 +1,7 @@
+export default ({ title }) => {
+  return (
+    <div className="container mx-auto max-w-screen-sm bg-teal-300 p-4 items-center fixed top-0 z-10">
+      <div className=" text-slate-900 text-xl">{title}</div>
+    </div>
+  );
+};
