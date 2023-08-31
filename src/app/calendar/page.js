61b552fc6b15bee6d7970752bf476710dd39e304
Henry Li <lilei105@hotmail.com>
2023-08-31 19:15:46 +0800

    日历视图重构

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -12,10 +12,37 @@ const Collapsible = dynamic(() => import('react-collapsible'), {
   ssr: false,
 });
 
-export default () => {
+const Collap = ({ title }) => {
+  return (
+    <Collapsible
+      open={true}
+      className=" bg-slate-500 text-xl p-2 "
+      openedClassName="bg-slate-300"
+      transitionTime={100}
+      trigger={<div>&#10148; {title}</div>}
+      triggerWhenOpen={<div>&#11167; {title}</div>}
+      triggerTagName="div"
+      triggerOpenedClassName=" bg-slate-500 text-xl p-2 "
+      // triggerElementProps={{ id: '123456' }}
+    >
+      <Link href="/list">
+        <div className="ml-4">
+          <p>1. This is the collapsible content.</p>
+          <p>2. This is the collapsible content.</p>
+          <p>3. This is the collapsible content.</p>
+          <p>4. This is the collapsible content.</p>
+          <p>5. This is the collapsible content.</p>
+          <p>6. This is the collapsible content.</p>
+        </div>
+      </Link>
+    </Collapsible>
+  );
+};
+
+export default function CalendarView() {
   return (
     <div>
-      <Headline title="选择一个日期，查看该日的歌单" />
+      <Headline title="选择一个日期，查看该日歌单" />
 
       <div className="mt-20 flex justify-center pb-4 border-b-4">
         <div className="border border-gray-200">
@@ -24,48 +51,8 @@ export default () => {
       </div>
 
       <div className="px-4 mt-2 divide-y-2">
-        <Collapsible
-          open="true"
-          className=" bg-slate-500 text-xl p-2 "
-          openedClassName="bg-slate-300"
-          transitionTime="100"
-          trigger="&#10148; 2023年9月3日 主日敬拜"
-          triggerWhenOpen="&#11167; 2023年9月3日 主日敬拜"
-          triggerTagName="div"
-          triggerOpenedClassName=" bg-slate-500 text-xl p-2 "
-          // triggerElementProps={{ id: '123456' }}
-        >
-          <Link href="/list">
-            <div className="ml-4">
-              <p>1. This is the collapsible content.</p>
-              <p>2. This is the collapsible content.</p>
-              <p>3. This is the collapsible content.</p>
-              <p>4. This is the collapsible content.</p>
-              <p>5. This is the collapsible content.</p>
-              <p>6. This is the collapsible content.</p>
-            </div>
-          </Link>
-        </Collapsible>
-
-        <Collapsible
-          className=" bg-slate-500 text-xl p-2"
-          openedClassName="bg-slate-300"
-          transitionTime="100"
-          trigger="&#10148; 2023年9月3日 祷告会"
-          triggerWhenOpen="&#11167; 2023年9月3日 祷告会"
-          triggerTagName="div"
-          triggerOpenedClassName=" bg-slate-500 text-xl p-2"
-          triggerElementProps={{ id: '789123' }}
-        >
-          <div className="ml-4">
-            <p>1. This is the collapsible content.</p>
-            <p>2. This is the collapsible content.</p>
-            <p>3. This is the collapsible content.</p>
-            <p>4. This is the collapsible content.</p>
-            <p>5. This is the collapsible content.</p>
-            <p>6. This is the collapsible content.</p>
-          </div>
-        </Collapsible>
+        <Collap title="2023年9月3日 主日敬拜" />
+        <Collap title="2023年9月3日 祷告会" />
       </div>
 
       <div className="mt-24"></div>
@@ -73,4 +60,4 @@ export default () => {
       <Footer title="回到首页" />
     </div>
   );
-};
+}
