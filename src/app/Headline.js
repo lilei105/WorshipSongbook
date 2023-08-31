fd885842585e98e3d4bb0f55efb74540af42c631
Henry Li <lilei105@hotmail.com>
2023-08-31 19:18:25 +0800

    标题栏字体加大

--- src/app/Headline.js
+++ src/app/Headline.js
@@ -1,7 +1,7 @@
-export default ({ title }) => {
+export default function Headline({ title }) {
   return (
     <div className="container mx-auto max-w-screen-sm bg-teal-300 p-4 items-center fixed top-0 z-10">
-      <div className=" text-slate-900 text-xl">{title}</div>
+      <div className=" text-slate-900 text-2xl">{title}</div>
     </div>
   );
-};
+}
