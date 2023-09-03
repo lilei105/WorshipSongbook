e4b33b8d582afa3d402dcf5c05e2d6e645b95ea1
“Henry <lilei105@hotmail.com>
2023-09-04 00:48:37 +0800

    列表页面可以从GET请求中获得参数

--- src/app/list/page.js
+++ src/app/list/page.js
@@ -1,50 +1,33 @@
-'use client';
+"use client";
 
-import Headline from '../Headline';
-import Image from 'next/image';
-import Footer from '../Footer';
-import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import Headline from "../Headline";
+import Image from "next/image";
+import Footer from "../Footer";
+import Link from "next/link";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import axios from "axios";
 
 export default function SongList() {
-  const images = [
-    {
-      name: '歌名很长歌名很长歌名很长歌名很长',
-      url: '/images/1.png',
-    },
-    {
-      name: '让赞美飞扬',
-      url: '/images/2.png',
-    },
-    {
-      name: '十字架',
-      url: '/images/3.png',
-    },
-    {
-      name: '唯一的安慰',
-      url: '/images/4.png',
-    },
-    {
-      name: '我需要有你在我生命中',
-      url: '/images/5.png',
-    },
-    {
-      name: '他医治破碎心灵',
-      url: '/images/6.png',
-    },
-    {
-      name: '丰盛的应许',
-      url: '/images/7.png',
-    },
-  ];
+  const [songs, setSongs] = useState([]);
 
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const listid = searchParams.get("songlistid");
+  console.log("query params: ", listid);
+
+  axios
+    .get(`/api/getByList?songlistid=${listid}`)
+    .then((res) => {
+      console.log("songs: ", res.data);
+      setSongs(res.data["data"]);
+    })
+    .catch((error) => console.error(error));
 
   const handleClick = (image) => {
     console.log(image);
     setTimeout(() => {
-      console.log('Paused for 1 second');
+      console.log("Paused for 1 second");
     }, 1000);
   };
 
@@ -53,15 +36,15 @@ export default function SongList() {
       <Headline title="2023年9月3日 主日敬拜" />
 
       <div className="pt-16 grid grid-cols-2 pb-20">
-        {images.map((image, index) => (
+        {songs.map((image, index) => (
           <div key={index} className=" relative">
-            <Link href={'/detail'}>
+            <Link href={"/detail"}>
               <div className=" bg-blue-300 border border-zinc-300 p-2 absolute top-0 left-0 w-full ">
-                {image['name']}
+                {image["name"]}
               </div>
 
               <div className="h-auto m-1 ">
-                <img src={image['url']} alt="歌谱图片" />
+                <img src={image["sheet_url"]} alt="歌谱图片" />
               </div>
             </Link>
           </div>
