0ac35065d9bd3d1825497f5f55e85c222496102f
Henry Li <lilei105@hotmail.com>
2023-09-04 15:33:10 +0800

    优化列表页，可以读取数据库里的图片链接了

--- src/app/list/page.js
+++ src/app/list/page.js
@@ -1,50 +1,46 @@
-"use client";
+'use client';
 
-import { useState, useEffect } from "react";
-import Headline from "../Headline";
-import Image from "next/image";
-import Footer from "../Footer";
-import Link from "next/link";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import axios from "axios";
+import { useState, useEffect } from 'react';
+import Headline from '../Headline';
+import Image from 'next/image';
+import Footer from '../Footer';
+import Link from 'next/link';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import axios from 'axios';
 
-export default function SongList() {
+export default function ShowSongsInList() {
   const [songs, setSongs] = useState([]);
 
   const router = useRouter();
   const searchParams = useSearchParams();
-  const listid = searchParams.get("songlistid");
-  console.log("query params: ", listid);
+  const title = searchParams.get('title');
+  const listId = searchParams.get('listId');
+  // console.log('query params: ', listId);
 
-  axios
-    .get(`/api/getByList?songlistid=${listid}`)
-    .then((res) => {
-      console.log("songs: ", res.data);
-      setSongs(res.data["data"]);
-    })
-    .catch((error) => console.error(error));
-
-  const handleClick = (image) => {
-    console.log(image);
-    setTimeout(() => {
-      console.log("Paused for 1 second");
-    }, 1000);
-  };
+  useEffect(() => {
+    axios
+      .get(`/api/getByList?listId=${listId}`)
+      .then((res) => {
+        console.log('songs: ', res.data);
+        setSongs(res.data['data']);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <div>
-      <Headline title="2023年9月3日 主日敬拜" />
+      <Headline title={title} />
 
       <div className="pt-16 grid grid-cols-2 pb-20">
         {songs.map((image, index) => (
-          <div key={index} className=" relative">
-            <Link href={"/detail"}>
+          <div key={index} className="relative">
+            <Link href={'/detail'}>
               <div className=" bg-blue-300 border border-zinc-300 p-2 absolute top-0 left-0 w-full ">
-                {image["name"]}
+                {image['name']}
               </div>
 
               <div className="h-auto m-1 ">
-                <img src={image["sheet_url"]} alt="歌谱图片" />
+                <img src={image['sheet_url']} alt="歌谱图片" />
               </div>
             </Link>
           </div>
