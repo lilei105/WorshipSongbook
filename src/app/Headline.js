43304654a7d6694f1ce340b07b1839e09ed5c9dd
Henry Li <lilei105@hotmail.com>
2023-09-04 15:31:17 +0800

    针对长标题修改标题栏样式

--- src/app/Headline.js
+++ src/app/Headline.js
@@ -1,7 +1,9 @@
 export default function Headline({ title }) {
   return (
     <div className="container mx-auto max-w-screen-sm p-4 items-center fixed top-0 z-10 bg-slate-200">
-      <div className=" text-slate-900 text-2xl">{title}</div>
+      <div className="whitespace-nowrap overflow-x-auto text-slate-900 text-2xl">
+        {title}
+      </div>
     </div>
   );
 }
