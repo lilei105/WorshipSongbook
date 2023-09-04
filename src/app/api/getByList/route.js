9893189890ad9cc89293aa14b260bd1e5ba9eed2
Henry Li <lilei105@hotmail.com>
2023-09-04 15:30:08 +0800

    修改参数名

--- src/app/api/getByList/route.js
+++ src/app/api/getByList/route.js
@@ -1,10 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export async function GET(req) {
-  const listID = parseInt(req.nextUrl.searchParams.get("songlistid"));
+  const listID = parseInt(req.nextUrl.searchParams.get('listId'));
   //console.log("listID = ", listID);
 
   const songs = await prisma.song.findMany({
