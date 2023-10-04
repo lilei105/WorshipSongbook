88dd213bcca399c863a25b4bb43309d8401687a5
“Henry <lilei105@hotmail.com>
2023-10-04 14:22:44 +0800

    改用handleClick事件处理点击，在事件中使用session storage保存点击信息

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -1,44 +1,44 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import Headline from '../Headline';
-import Footer from '../Footer';
-import dynamic from 'next/dynamic';
-import Link from 'next/link';
-import dayjs from 'dayjs';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import Headline from "../Headline";
+import Footer from "../Footer";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+import dayjs from "dayjs";
+import axios from "axios";
 // import Collapsible from 'react-collapsible';
 
-const Collapsible = dynamic(() => import('react-collapsible'), {
+const Collapsible = dynamic(() => import("react-collapsible"), {
   ssr: false,
 });
 
 function isNullOrEmpty(value) {
-  return value === null || value === undefined || value === '';
+  return value === null || value === undefined || value === "";
 }
 
 //根据API传来的数据生成多个可折叠的内容
 const CollapContent = ({ content }) => {
   // console.log('content fetched from api is: ', content);
-  let title = '';
+  let title = "";
   let songlistId = 0;
   let songs = [];
-  let str = '';
+  let str = "";
 
   if (isNullOrEmpty(content) || content.length < 1) {
-    return CollapItem('今日没有数据', 0, [], 0);
+    return CollapItem("今日没有数据", 0, [], 0);
   }
 
   //有多少个list就生成多少串Collapsible，最后一起返回
   function buildData(content) {
     return content.map(
       (list, index) => (
-        (title = dayjs(list.date).format('YYYY年MM月DD日') + ' ' + list.name),
+        (title = dayjs(list.date).format("YYYY年MM月DD日") + " " + list.name),
         // console.log(title),
         (songlistId = list.id),
-        (songs = list['songs']),
+        (songs = list["songs"]),
         CollapItem(title, songlistId, songs, index)
       )
     );
@@ -49,6 +49,21 @@ const CollapContent = ({ content }) => {
 
   return buildData(content);
 };
+
+//处理点击事件，向/list页面POST列表ID和标题
+function handleClick(e, songlistId, title) {
+  // e.preventDefault();
+  // console.log(title);
+
+  if (!isNullOrEmpty(window)) {
+    window.sessionStorage.setItem("songlistId", songlistId);
+    window.sessionStorage.setItem("title", title);
+  }
+
+  // axios.get("/list").catch((error) => {
+  //   console.error(error);
+  // });
+}
 
 //只管按可折叠的格式显示标题和内容
 const CollapItem = (title, songlistId, songs, index) => (
@@ -63,7 +78,8 @@ const CollapItem = (title, songlistId, songs, index) => (
     triggerOpenedClassName=" bg-slate-500 text-xl p-2 "
     key={index}
   >
-    <Link href={'/list?listId=' + songlistId + '&title=' + title}>
+    {/* <Link href={'/list?listId=' + songlistId + '&title=' + title}> */}
+    <Link href="/list" onClick={(e) => handleClick(e, songlistId, title)}>
       <div className="ml-4">
         {songs.map((item, index) => (
           <p key={index}>
@@ -77,14 +93,14 @@ const CollapItem = (title, songlistId, songs, index) => (
 
 export default function CalendarView() {
   const [selectedDate, setSelectedDate] = useState(
-    dayjs(new Date()).format('YYYY-MM-DD')
+    dayjs(new Date()).format("YYYY-MM-DD")
   );
   const [data, setData] = useState();
 
   //处理鼠标点击的事件，将所选日期格式化为YYYY-MM-DD格式，
   //改变selectedDate，触发useEffect
   function handleSelectDateChanged(value) {
-    setSelectedDate(dayjs(value).format('YYYY-MM-DD'));
+    setSelectedDate(dayjs(value).format("YYYY-MM-DD"));
   }
 
   //使用useEffect，每当selectedDate改变时触发api查询，
